aef50ddb0cda8226bfd37662a148a866893d74e1
pederengelstad <39351080+pederengelstad@users.noreply.github.com>
2018-11-02 16:05:20 -0600

    something funky happening with the gbif filtering

--- SpeciesOccurrenceData/API_Sources.R
+++ SpeciesOccurrenceData/API_Sources.R
@@ -59,8 +59,15 @@ api_data <- function(species_list = NULL, sources=c('gbif','bison','inat','ecoen
     ))
     
     if(sum(sapply(spocc_df$gbif$data, NROW)) > 0){
-      gbif_df <- as.data.frame(occ2df(spocc_df$gbif)) %>%
-        filter(!({if('habitat' %in% names(.)) habitat} %in% 'cultivated') & basisOfRecord %in% c("PRESERVED_SPECIMEN", "HUMAN_OBSERVATION", "OBSERVATION")) %>%
+      gbif_df <- as.data.frame(occ2df(spocc_df$gbif))
+      
+      if('habitat' %in% names(gbif_df)){
+        gbif_df = gbif_df %>%
+          filter(!(habitat %in% 'cultivated'))
+      }
+
+      gbif_df %>%
+        filter(basisOfRecord %in% c("PRESERVED_SPECIMEN", "HUMAN_OBSERVATION", "OBSERVATION")) %>%
         select(prov, name, longitude, latitude, eventDate, year, scientificName) %>%
         mutate(DataSet = prov,
                decimalLatitude = as.numeric(latitude),

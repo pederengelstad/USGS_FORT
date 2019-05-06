98050142b75f0fbc440a0f44c79cf81a203fcdb4
pederengelstad <39351080+pederengelstad@users.noreply.github.com>
2019-05-06 16:00:38 -0600

    change to USDA filtering and source

--- SpeciesOccurrenceData/SpeciesProcessing.R
+++ SpeciesOccurrenceData/SpeciesProcessing.R
@@ -58,52 +58,42 @@ species_processing <- function(sp_list=NULL, USDA=TRUE){
     unique()
   sp_df = sp_df[!sp_df$synonym_base %in% grep("X", sp_df$synonym_base, value = T, ignore.case = F),]
 
-  
-  # sometimes, the only matches are for outdated unaccepted terms or the species name may be misspelled.
-  # still, it's good to include it, in case lots of sources use this term and ITIS doesn't for whatever reason.
-  # if(!is.null(sp_list[!sp_list %in% c(unique(na.omit(s$ITISacceptedName)),unique(na.omit(s$synonym_base)))])){
-  #   for(i in sp_list[!sp_list %in% c(unique(na.omit(s$ITISacceptedName)),unique(na.omit(s$synonym_base)))]){
-  #     sp_df = rbind(sp_df,c(paste0(i," sp."),i,NA))
-  #   }
-  # }
-
   sp_df <- sp_df[(sp_df$ITISacceptedName!=sp_df$synonym_base | is.na(sp_df$ITISacceptedName==sp_df$synonym_base)),]
   sp_df <<- sp_df[order(sp_df$ITISacceptedName),]
   
   # synthesize full, unique species name list including synonyms
   species_search_list <<- sort(unique(na.omit(c(sp_df$ITISacceptedName, sp_df$synonym_base))))
   
-  # to search the USDA plants database for codes, two queries are generated:
-  # 1. searching for the currently used code
-  # 2. searching for all previously used code synonyms
+  # search the USDA plants database (update 2019-05-06) for their codes
   if(USDA == TRUE){
     
     print("Finding USDA species codes")
     
-    plants.db = as.data.frame(gsheet::gsheet2tbl("https://docs.google.com/spreadsheets/d/1OCer0LGtsVjAcjNrIa6aQQ_7YzHycemO1rwI-aJePr4/edit?usp=sharing"))
+    plants.db = as.data.frame(gsheet::gsheet2tbl("https://docs.google.com/spreadsheets/d/1WkSt3EcOUkiRPRWeEZTKuQvkjhYFQre_Ox-I3FpPHbA/edit?usp=sharing"))
 
     #First check for matches with ITIS accepted names
     acc.symbol.df = plants.db[plants.db$Scientific_Name %in% sp_df$ITISacceptedName,]
     syn.symbol.df = plants.db[plants.db$Scientific_Name %in% sp_df$synonym_base[!is.na(sp_df$synonym_base)],]
 
     #Then check through the synonyms (there should be less of these, generally)
-    for(i in 1:nrow(sp_df)){
-      
-      if(!is.na(sp_df[i, 1])){
-        acc.df <- plants.db[plants.db$Scientific_Name == sp_df[i, 1],]
-        acc.df.full <- plants.db[plants.db$Accepted_Symbol == acc.df[[1]],]
-        symbols.flat <- unique(c(acc.df.full[,1], acc.df.full[,2], acc.df.full[,3]))
-        symbols.vec <- symbols.flat[!is.na(symbols.flat)]
-      }
-      
-      if(!is.na(sp_df[i, 2])){
-        syn.symbol <- syn.symbol.df[syn.symbol.df$Scientific_Name == sp_df[i,2],][[3]]
-        symbols.vec <- unique(c(symbols.vec,syn.symbol))
-      }
-      
-      symbols.vec <- str_flatten(symbols.vec, collapse = ',')
-      sp_df$usda_codes[i] <<- ifelse(length(symbols.vec) == 0, NA, symbols.vec)
+  for(i in 1:nrow(sp_df)){
+    
+    if(!is.na(sp_df[i, 1])){
+      acc.df <- plants.db[plants.db$Scientific_Name == sp_df[i, 1],]
+      acc.df.full <- plants.db[plants.db$Accepted_Symbol == acc.df[[1]],]
+      symbols.flat <- unique(c(acc.df.full[,1], acc.df.full[,2]))
+      symbols.vec <- symbols.flat[!is.na(symbols.flat)]
     }
+    
+    if(!is.na(sp_df[i, 2])){
+      syn.symbol <- syn.symbol.df[syn.symbol.df$Scientific_Name == sp_df[i,2],][[2]]
+      symbols.vec <- unique(c(symbols.vec,syn.symbol))
+    }
+    
+    symbols.vec <- str_flatten(symbols.vec, collapse = ',')
+    sp_df$usda_codes[i] <<- ifelse(length(symbols.vec) == 0, NA, symbols.vec)
+  }
+
 
     rm(plants.db)
   
